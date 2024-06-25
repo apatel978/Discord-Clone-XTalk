3de37aca06ef0bbb93387e0f4b96723191def0df
Oleh Paris <olehparis@gmail.com>
2024-06-25 15:26:18 -0400

    login complete

--- aa19-python-group-project/react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ aa19-python-group-project/react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -27,16 +27,31 @@ function LoginFormModal() {
       closeModal();
     }
   };
-
+  const isSubmitDisabled = email.length < 4 || password.length < 6;
+  const handleDemoUserLogin = async () => {
+    setEmail('demo@aa.io');
+    setPassword('password');
+    try {
+      // Attempt to log in with demo user credentials
+      await dispatch(thunkLogin.login({ credential: 'demo@aa.io', password: 'password' }));
+      closeModal(); 
+    } catch (error) {
+      if (error.status === 401)
+        setErrors({ invalidCredentials: 'The provided credentials were invalid' });
+    }
+  };
   return (
-    <>
-      <h1>Log In</h1>
+    
+    <div className='modal-login'>
+      <h1>Welcome Back!</h1>
+      <h5>We are excited to see you again!</h5>
       <form onSubmit={handleSubmit}>
         <label>
-          Email
+          <div>Email or Username</div>
           <input
             type="text"
             value={email}
+            placeholder="email@domain.com"
             onChange={(e) => setEmail(e.target.value)}
             required
           />
@@ -47,14 +62,18 @@ function LoginFormModal() {
           <input
             type="password"
             value={password}
+            placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
             required
           />
         </label>
         {errors.password && <p>{errors.password}</p>}
-        <button type="submit">Log In</button>
+        <div className="buttons-container">
+        <button type="submit" className="demo-user-button" disabled={isSubmitDisabled}>Log In</button>
+        <button onClick={handleDemoUserLogin} className="demo-user-button">Demo User</button>
+        </div>
       </form>
-    </>
+      </div>
   );
 }
 
