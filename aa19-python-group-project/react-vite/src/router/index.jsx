58e9bd55f59c3a22ce18022f6801e3bc628ddb34
Oleh Paris <olehparis@gmail.com>
2024-06-27 20:33:25 -0400

    bug fix

--- aa19-python-group-project/react-vite/src/router/index.jsx
+++ aa19-python-group-project/react-vite/src/router/index.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import LoginFormPage from '../components/LoginFormPage';
-import SignupFormPage from '../components/SignupFormPage';
+
 import Layout from './Layout';
 
 export const router = createBrowserRouter([
@@ -9,15 +8,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Welcome!</h1>,
-      },
-      {
-        path: "login",
-        element: <LoginFormPage />,
-      },
-      {
-        path: "signup",
-        element: <SignupFormPage />,
+        element: <h1>Welcome Diana</h1>,
       },
     ],
   },
