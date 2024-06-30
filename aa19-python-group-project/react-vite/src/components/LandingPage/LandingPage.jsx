36083076f9346901ba54dee08268c8ae56c1ac8e
Diana Le <dianapnle@gmail.com>
2024-06-29 20:38:11 -0700

    adjusted paragraph landing page and added normalize css

--- aa19-python-group-project/react-vite/src/components/LandingPage/LandingPage.jsx
+++ aa19-python-group-project/react-vite/src/components/LandingPage/LandingPage.jsx
@@ -3,18 +3,18 @@ import './LandingPage.css';
 
 const LandingPage = () => {
   return (<> <div className='lph1'>
-      <h1>CONNECT, CHAT, AND PLAY TOGETHER</h1>
+      <h1>CONNECT AND CHAT TOGETHER</h1>
       </div>
     <div className="landing-page">
-      
+
       <div className="text-container">
         <p>
 
 
-Our platform is perfect for gaming sessions, casual hangouts, or building a global community. Whether you&apos;re a dedicated gamer, a social butterfly, or a community organizer, our platform offers the ideal environment to meet your needs. Create your personalized space to chat, play, and enjoy quality time with friends.
+Our platform is perfect for casual hangouts and connecting with communities all over the world. Create your personalized space to chat and create connections with old and new friends.
 
 </p>
-<p>For gamers, we provide specialized features to enhance your gaming experience. Coordinate with your team using dedicated gaming channels, share your screen for strategy discussions, and enjoy high-quality voice and video calls that make you feel like you’re in the same room. </p>
+<p></p>
       </div>
       <div className="image-container">
         <img src="../../../images/landing2.jpg" alt="Landing" />
