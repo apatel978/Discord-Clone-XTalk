94cbbdab0d76f71df817a68546187d4ba72123df
Akshar <aksharpatel978@gmail.com>
2024-07-08 19:50:07 -0400

    made server details component - struggling with get server by id

--- aa19-python-group-project/react-vite/src/redux/servers.js
+++ aa19-python-group-project/react-vite/src/redux/servers.js
@@ -1,10 +1,16 @@
 import { csrfFetch } from "./csrf";
 
 const GET_ALL_SERVERS = "servers/getServers";
+const GET_SERVER_BY_ID = "servers/getServerById";
 
 const getAllServers = (servers) => ({
   type: GET_ALL_SERVERS,
   payload: servers,
+});
+
+const getServerById = (server) => ({
+  type: GET_SERVER_BY_ID,
+  payload: server
 });
 
 export const thunkGetAllServers = () => async (dispatch) => {
@@ -18,21 +24,41 @@ export const thunkGetAllServers = () => async (dispatch) => {
   }
 };
 
+export const thunkServerById = (serverId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/servers/${serverId}`);
+  if (res.ok) {
+    const server = await res.json();
+    if (server.errors) {
+      return;
+    }
+    dispatch(getServerById(server))
+    return server;
+  }
+};
+
 const initialState = {};
 
 function serversReducer(state = initialState, action) {
     switch (action.type) {
-        case GET_ALL_SERVERS:
-            let nextState = {};
-            Object.entries(action.payload.Servers).forEach(([key, value]) => {
-                  nextState[value.id] = value;
-            })
-            return {
-                ...state,
-                ...nextState
-            };
-        default:
-            return state;
+      case GET_ALL_SERVERS: {
+        let nextState = {};
+        Object.entries(action.payload.Servers).forEach(([key, value]) => {
+              console.log('keys', key)
+              nextState[value.id] = value;
+        })
+        return {
+          ...state,
+          ...nextState
+        };
+      }
+      case GET_SERVER_BY_ID: {
+        const cloneState = structuredClone(state);
+        const server = action.payload;
+        console.log('SERVER: ', server);
+        return cloneState;
+      }
+      default:
+        return state;
     }
 }
 
