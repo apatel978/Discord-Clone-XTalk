5ad91a7f6a08125caef6e34d37e1ef4d58ca5197
Diana Le <dianapnle@gmail.com>
2024-07-15 16:10:56 -0700

    updated the leave server name

--- aa19-python-group-project/react-vite/src/components/ServerInfo/LeaveServerModal.jsx
+++ aa19-python-group-project/react-vite/src/components/ServerInfo/LeaveServerModal.jsx
@@ -4,12 +4,11 @@ import { useState} from "react";
 import { useModal } from "../../context/Modal";
 import { thunkLeaveServer } from '../../redux/servers';
 
-const LeaveServerModal = (serverId) => {
-    const servId= Object.values(serverId)
+const LeaveServerModal = ({serverId}) => {
     const dispatch = useDispatch();
     const { closeModal } = useModal();
-    const username = useSelector((state)=> state.session.user.username);
-    const [serverName] = useState(`${username}'s Server`);
+    const server = useSelector((state)=> state.servers[serverId]);
+
 
     const handleLeave = async (e) => {
         e.preventDefault();
@@ -18,15 +17,15 @@ const LeaveServerModal = (serverId) => {
     }
     return (
         <div className='modal-login'>
-        <h1>Leave {serverName}?</h1>
-        <h5>Your are about to leave {serverName}, are you sure?</h5>
-        
+        <h1>Leave {server.name}?</h1>
+        <h5>Your are about to leave {server.name}, are you sure?</h5>
+
 
           <div className="buttons-container">
           <button onClick={closeModal} className="submit-btn" >No</button>
         <button onClick={handleLeave} className="button-leave" >Leave</button>
         </div>
-        
+
         <span className="sp sp-t"></span>
 			<span className="sp sp-r"></span>
 			<span className="sp sp-b"></span>
