269044d932594e3316bd65e21ec184eb4c4e79f2
Oleh Paris <olehparis@gmail.com>
2024-07-18 20:30:48 -0400

    late

--- aa19-python-group-project/app/__init__.py
+++ aa19-python-group-project/app/__init__.py
@@ -20,14 +20,12 @@ from .config import Config
 from .models.message import Message
 from app.websocket import socketio, setup_socketio, authenticated_only
 
-
-
 app = Flask(__name__, static_folder='../react-vite/dist', static_url_path='/')
-CORS(app, resources={r"/*": {"origins": "*"}})  # Enable CORS for all routes
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 # Setup login manager
 login = LoginManager(app)
 login.login_view = 'auth.unauthorized'
-
 
 @login.user_loader
 def load_user(id):
@@ -35,7 +33,6 @@ def load_user(id):
 
 # Setup SocketIO
 setup_socketio(app, cors_allowed_origins="*")
-
 
 # Tell flask about our seed commands
 app.cli.add_command(seed_commands)
@@ -50,16 +47,11 @@ app.register_blueprint(retrieve_routes, url_prefix='/api')
 app.register_blueprint(message_routes, url_prefix='/api/messages')
 app.register_blueprint(reaction_routes, url_prefix='/api/reactions')
 db.init_app(app)
-Migrate(app, db)
+migrate = Migrate(app, db)
 
 # Application Security
 CORS(app)
 
-
-# Since we are deploying with Docker and Flask,
-# we won't be using a buildpack when we deploy to Heroku.
-# Therefore, we need to make sure that in production any
-# request made over http is redirected to https.
 @app.before_request
 def https_redirect():
     if os.environ.get('FLASK_ENV') == 'production':
@@ -68,50 +60,37 @@ def https_redirect():
             code = 301
             return redirect(url, code=code)
 
-
 @app.after_request
 def inject_csrf_token(response):
     response.set_cookie(
         'csrf_token',
         generate_csrf(),
         secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
-        samesite='Strict' if os.environ.get(
-            'FLASK_ENV') == 'production' else None,
-        httponly=True)
+        samesite='Strict' if os.environ.get('FLASK_ENV') == 'production' else None,
+        httponly=True
+    )
     return response
-
 
 @app.route("/api/docs")
 def api_help():
-    """
-    Returns all API routes and their doc strings
-    """
     acceptable_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    route_list = { rule.rule: [[ method for method in rule.methods if method in acceptable_methods ],
-                    app.view_functions[rule.endpoint].__doc__ ]
-                    for rule in app.url_map.iter_rules() if rule.endpoint != 'static' }
+    route_list = {
+        rule.rule: [[method for method in rule.methods if method in acceptable_methods],
+                    app.view_functions[rule.endpoint].__doc__]
+        for rule in app.url_map.iter_rules() if rule.endpoint != 'static'
+    }
     return route_list
-
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
-    """
-    This route will direct to the public directory in our
-    react builds in the production environment for favicon
-    or index.html requests
-    """
     if path == 'favicon.ico':
         return app.send_from_directory('public', 'favicon.ico')
     return app.send_static_file('index.html')
 
-
 @app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
-
-
-
 
 @socketio.on('join')
 def on_join(data):
@@ -120,10 +99,6 @@ def on_join(data):
     channel = data['channel']
     join_room(channel)
     print('joined room')
-
-
-
-
 
 @socketio.on('leave')
 @authenticated_only
@@ -134,15 +109,13 @@ def on_leave(data):
     leave_room(channel)
     print('left room')
 
-
 @socketio.on('message')
 @authenticated_only
 def handle_message(data):
     print(f'hello from message: {data}')
     channel = data['channel']
     message = data['message']
-
-    new_message = Message (message=message, channel_id=channel, user_id=current_user.id)
+    new_message = Message(message=message, channel_id=channel, user_id=current_user.id)
     db.session.add(new_message)
     db.session.commit()
     emit('message', new_message.to_dict(), to=channel)
