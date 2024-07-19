62d8302c242bf147e4f3d69a0bf774c1829ab619
Oleh Paris <olehparis@gmail.com>
2024-07-18 23:48:08 -0400

    updated modals migrations

--- aa19-python-group-project/app/models/__init__.py
+++ aa19-python-group-project/app/models/__init__.py
@@ -1,8 +1,8 @@
 from .db import db
 from .user import User
-from .reaction import Reaction
+from .server import Server
 from .channel import Channel
 from .member import Member
 from .message import Message
-from .server import Server
+from .reaction import Reaction
 from .db import environment, SCHEMA
