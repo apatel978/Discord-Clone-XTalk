37f9c67386750c589d66ed6173a35b490f98dba8
Diana Le <dianapnle@gmail.com>
2024-07-21 16:11:40 -0700

    updated models

--- aa19-python-group-project/app/models/channel.py
+++ aa19-python-group-project/app/models/channel.py
@@ -1,28 +1,28 @@
-# from .db import db, environment, SCHEMA, add_prefix_for_prod
-# from datetime import datetime
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
 
-# class Channel(db.Model):
-#     __tablename__= 'channels'
+class Channel(db.Model):
+    __tablename__= 'channels'
 
-#     if environment == "production":
-#         __table_args__ = {'schema': SCHEMA}
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     server_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('servers.id')), nullable=False)
-#     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-#     created_at = db.Column(db.DateTime, default=datetime.now())
-#     updated_at = db.Column(db.DateTime, default=datetime.now())
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    server_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('servers.id')), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())
 
-#     owner = db.relationship('User', back_populates='channel_user')
-#     server = db.relationship('Server', back_populates='server_channels')
-#     messages = db.relationship('Message', back_populates='channel', cascade='all, delete-orphan')
+    owner = db.relationship('User', back_populates='channel_user')
+    server = db.relationship('Server', back_populates='server_channels')
+    messages = db.relationship('Message', back_populates='channel', cascade='all, delete-orphan')
 
-#     def to_dict(self):
-#         return {
-#             'id': self.id,
-#             'serverId': self.server_id,
-#             'userId': self.user_id,
-#             'name': self.name,
-#             'createdAt': self.created_at
-#         }
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'serverId': self.server_id,
+            'userId': self.user_id,
+            'name': self.name,
+            'createdAt': self.created_at
+        }
--- aa19-python-group-project/app/models/member.py
+++ aa19-python-group-project/app/models/member.py
@@ -1,13 +1,12 @@
-# from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
-# class Member(db.Model):
-#     __tablename__ = 'members'
+class Member(db.Model):
+    __tablename__ = 'members'
 
-#     if environment == "production":
-#         __table_args__ = {'schema': SCHEMA}
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     server_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('servers.id')), nullable=False)
-#     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    
+    id = db.Column(db.Integer, primary_key=True)
+    server_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('servers.id')), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
