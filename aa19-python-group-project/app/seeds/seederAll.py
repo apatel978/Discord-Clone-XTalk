90178959a2e112e7ac90968a1cb6e26a8c58ece0
Akshar <aksharpatel978@gmail.com>
2024-09-22 14:32:47 -0400

    changed preview pic

--- aa19-python-group-project/app/seeds/seederAll.py
+++ aa19-python-group-project/app/seeds/seederAll.py
@@ -19,11 +19,11 @@ def undo_users():
     db.session.commit()
 
 def seed_servers():
-    demoServer = Server(name='DemoServer', owner_id=1, preview='https://i1.sndcdn.com/artworks-000666620167-6kjqim-t500x500.jpg')
-    anotherDemo = Server(name='Test2', owner_id=2, preview='https://i1.sndcdn.com/artworks-000666620167-6kjqim-t500x500.jpg')
-    DianaDemo = Server(name='Diana', owner_id=3, preview='https://i1.sndcdn.com/artworks-000666620167-6kjqim-t500x500.jpg')
-    OlehDemo = Server(name='Oleh', owner_id=1, preview='https://i1.sndcdn.com/artworks-000666620167-6kjqim-t500x500.jpg')
-    AksharDemo = Server(name='Akshar', owner_id=2, preview='https://i1.sndcdn.com/artworks-000666620167-6kjqim-t500x500.jpg')
+    demoServer = Server(name='DemoServer', owner_id=1, preview='https://static.vecteezy.com/system/resources/previews/003/673/767/original/x-letter-logo-icon-for-business-and-company-vector.jpg')
+    anotherDemo = Server(name='Test2', owner_id=2, preview='https://static.vecteezy.com/system/resources/previews/003/673/767/original/x-letter-logo-icon-for-business-and-company-vector.jpg')
+    DianaDemo = Server(name='Diana', owner_id=3, preview='https://static.vecteezy.com/system/resources/previews/003/673/767/original/x-letter-logo-icon-for-business-and-company-vector.jpg')
+    OlehDemo = Server(name='Oleh', owner_id=1, preview='https://static.vecteezy.com/system/resources/previews/003/673/767/original/x-letter-logo-icon-for-business-and-company-vector.jpg')
+    AksharDemo = Server(name='Akshar', owner_id=2, preview='https://static.vecteezy.com/system/resources/previews/003/673/767/original/x-letter-logo-icon-for-business-and-company-vector.jpg')
 
     db.session.add(demoServer)
     db.session.add(anotherDemo)
